5b4528385f84d491a9a0d151d8086d66f5dc193d
Patricia Martinez <trish.on.a.mish@gmail.com>
2018-04-09 15:10:24 -0700

    working on functions

--- assets/javascript/liri.js
+++ assets/javascript/liri.js
@@ -21,33 +21,33 @@ var client = new Twitter(keys.twitter);
 
 //opening line runs when application opened
 function liriIntro() {
-    inquirer.prompt([{
+    inquirer.prompt({
         type: "list",
         name: "command",
         message: "\nHello! I'm Liri.\n I am here to assist you with obtaining information on movies, songs, and recent twitter posts.\n Please start by choosing a command that you would like to carry out from the options below.\n",
         choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-is-says"]
-    }])
+    }, ).then(function (user) {
+
+        //switch case for choosing which command to carry out 
+        switch (command) {
+            case "my-tweets":
+                myTweets();
+                break;
+
+            case "spotify-this-song":
+                spotifyThis(input);
+                break;
+
+            case "movie-this":
+                searchOmdb(input);
+                break;
+
+            case "do-what-it-says":
+                doWhatItSays();
+
+        };
+    });
 };
-
-
-//switch case for choosing which command to carry out 
-switch (command) {
-    case "my-tweets":
-        myTweets();
-        break;
-
-    case "spotify-this-song":
-        spotifyThis(input);
-        break;
-
-    case "movie-this":
-        searchOmdb(input);
-        break;
-
-    case "do-what-it-says":
-        doWhatItSays();
-
-}
 
 //display last 20 tweets, and when they were created
 function myTweets() {
@@ -56,53 +56,70 @@ function myTweets() {
         count: 20,
     };
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
-            if (!error) {
-                for (var i = 0; i < tweets.length; i++) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
 
-                    console.log("Here are your last twenty tweets, " + params.screen_name + "!");
-                    console.log(tweets[i].text);
-                    console.log(tweets[i].created_at);
-                    console.log("------------------------")
-
-                });
-
-        }
-
-    }
-
-
-    //node liri.js spotify-this-song 'song'
-    //display artist, song name, preview link, album that song is from, default "the sign" by ace of base 
-    function spotifyThis() {
-        console.log("Song: " + );
-        console.log("Artist: " + );
-        console.log("Click here to preview the song!");
-
-    }
-
-    //node liri.js movie-this 'movie'
-    //display title, year released, IMDB rating, rotten tomatoes rating, country produced in, language, plot, actors
-    //default "Mr. Nobody"
-    //key = trilogy
-    function searchOmdb() {
-        //        * Title of the movie.
-        //        * Year the movie came out.
-        //        * IMDB Rating of the movie.
-        //        * Rotten Tomatoes Rating of the movie.
-        //        * Country where the movie was produced.
-        //        * Language of the movie.
-        //        * Plot of the movie.
-        //        * Actors in the movie.
-    }
+                console.log("\nHere are your last twenty tweets, " + params.screen_name + "!\n");
+                console.log("------------------------");
+                console.log(tweets[i].text);
+                console.log(tweets[i].created_at);
+                console.log("------------------------");
+                // } else {
+                //     console.log("An error occured, please try again!")
+            };
+        };
+    });
+};
 
 
-    // 4. `node liri.js do-what-it-says`
+function spotifyThis() {
 
-    //    * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+    var spotifyInfo = data.tracks.items[i]
+    console.log("\nSong: " + spotifyInfo.name);
+    console.log("Artist: " + spotifyInfo.artists[0].name);
+    console.log("Album: " + spotifyInfo.album.name);
+    console.log("Preview here: " + )
 
-    //      * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
+}
 
-    //      * Feel free to change the text in that document to test out the feature for other commands.
-    function doWhatItSays() {
+//node liri.js spotify-this-song 'song'
+//display artist, song name, preview link, album that song is from, default "the sign" by ace of base 
 
-    }
+//node liri.js movie-this 'movie'
+//display title, year released, IMDB rating, rotten tomatoes rating, country produced in, language, plot, actors
+//default "Mr. Nobody"
+//key = trilogy
+
+function searchOmdb() {
+
+
+
+    console.log("\n" + data.Title);
+    console.log("------------------------");
+    console.log("Release Year: " + data.Year);
+    console.log("IMDB Rating: " + data.imdbRating);
+    console.log("Rotten Tomatoes Rating: " + data.Ratings[1].Value);
+    console.log("Film Country of Origin: " + data.Country);
+    console.log("Language: " + data.Language);
+    console.log("Plot: " + data.Plot);
+    console.log("Actors: " + data.Actors);
+}
+
+
+// 4. `node liri.js do-what-it-says`
+
+//    * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+
+//      * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
+
+//      * Feel free to change the text in that document to test out the feature for other commands.
+function doWhatItSays() {
+
+}
+
+function logData() {
+
+}
+
+
+liriIntro();
